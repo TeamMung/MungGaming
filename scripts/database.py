29e36d7972d6c4460872f8297c9c8e7620367153
Joe Baker <joeblakeb@gmail.com>
2022-11-23 13:55:46 +0000

    Add more details to docstrings

--- scripts/database.py
+++ scripts/database.py
@@ -18,7 +18,10 @@ class database:
         return con, cur
 
     def executeScript(self, filename):
-        """Execute a script file."""
+        """Execute a script file.
+        
+        Keyword arguments:
+        filename -- the name of the sql file to execute"""
         with open(os.path.join(os.path.dirname(__file__), filename), "r") as f:
             script = f.read()
         con, cur = self.connect()
@@ -51,7 +54,12 @@ class database:
         con.close()
 
     def getUser(self, username):
-        """Get a user from the database."""
+        """Get a user from the database.
+        Returns a dictionary of the user's details,
+        or None if the user does not exist.
+        
+        Keyword arguments:
+        username -- the username of the user to get"""
         con, cur = self.connect()
         cur.execute(
             "SELECT \
@@ -66,17 +74,25 @@ class database:
             WHERE users.username = ?", (username,))
         user = cur.fetchone()
         con.close()
-        return {
-            "username":     user[0],
-            "passwordHash": user[1],
-            "email":        user[2],
-            "dateOfBirth":  user[3],
-            "phoneNumber":  user[4],
-            "role": user[5]
-        }
+        try:
+            return {
+                "username":     user[0],
+                "passwordHash": user[1],
+                "email":        user[2],
+                "dateOfBirth":  user[3],
+                "phoneNumber":  user[4],
+                "role": user[5]
+            }
+        except TypeError:
+            return None
 
     def checkPassword(self, username, password):
-        """Check if a password is correct for a user."""
+        """Check if a password is correct for a user.
+        Returns True if the password is correct, False if not.
+        
+        Keyword arguments:
+        username -- the username of the user to check
+        password -- the password to check"""
         user = self.getUser(username)
         if user is None:
             return False
@@ -85,7 +101,11 @@ class database:
         return bcrypt.checkpw(password, passwordHash)
     
     def changeUserRole(self, username, role):
-        """Change a user's role."""
+        """Change a user's role.
+        
+        Keyword arguments:
+        username -- the username of the user to change
+        role     -- the new role of the user"""
         con, cur = self.connect()
         cur.execute(
             "UPDATE users SET roleID = (\

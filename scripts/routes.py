3b289c0fa16eb4c6af4459c83965bbe8a2f22ff1
Christopher Topp (s5414181) <s5414181@bournemouth.ac.uk>
2022-11-23 14:39:53 +0000

    Added registration page
    Added, the skeleton of a register page, and updated the routes to reflect it

--- scripts/routes.py
+++ scripts/routes.py
@@ -16,11 +16,34 @@ def index():
     """Send index page"""
     return flask.render_template("index.html")
 
-@gamelist.route("/login", endpoint="login", methods=["GET"])
-def index():
+@gamelist.route("/login", endpoint="login", methods=["GET", "POST"])
+def login():
     """Send log-in page"""
-    return flask.render_template("login.html")
 
+    if flask.request.method == 'POST':
+        username = flask.request.form['username']
+        password = flask.request.form['password']
+        
+        passwordValid = db.checkPassword(username, password) == False
+        return flask.render_template("login.html",
+            passwordValid=passwordValid)
+    else:
+        return flask.render_template("login.html", passwordValid=True)
+
+@gamelist.route("/",  endpoint="register", methods=["POST"])
+def register():
+
+    username = flask.request.form['username']
+    password = flask.request.form['password']
+    email = flask.request.form['email']
+    dob = flask.request.form['dob']
+    phonenumber = flask.request.form['phonenumber']
+
+    db.addUser(username, password, email, dob, phonenumber)
+
+
+    """Send register page"""
+    return flask.render_template("index.html")
 
 
 if "__main__" == __name__:

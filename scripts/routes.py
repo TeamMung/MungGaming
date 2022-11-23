560d50937d8f58f575675cadb75e9ba3a922c3bf
Christopher Topp (s5414181) <s5414181@bournemouth.ac.uk>
2022-11-23 14:48:00 +0000

    fixed routes
    fixed routes

--- scripts/routes.py
+++ scripts/routes.py
@@ -5,16 +5,17 @@ import flask
 gamelist = flask.Blueprint("gamelist", __name__, template_folder="templates")
 
 
-@gamelist.route("/static/<path:path>",  endpoint="static", methods=["GET"])
+@gamelist.route("/static/<path:path>", endpoint="static", methods=["GET"])
 def sendStatic(path):
     """Send all files in the static folder"""
     return flask.send_from_directory("static", path)
 
 
-@gamelist.route("/",  endpoint="index", methods=["GET"])
+@gamelist.route("/", endpoint="index", methods=["GET"])
 def index():
     """Send index page"""
     return flask.render_template("index.html")
+
 
 @gamelist.route("/login", endpoint="login", methods=["GET", "POST"])
 def login():
@@ -30,20 +31,20 @@ def login():
     else:
         return flask.render_template("login.html", passwordValid=True)
 
-@gamelist.route("/",  endpoint="register", methods=["POST"])
-def register():
 
-    username = flask.request.form['username']
-    password = flask.request.form['password']
-    email = flask.request.form['email']
-    dob = flask.request.form['dob']
-    phonenumber = flask.request.form['phonenumber']
+@gamelist.route("/register", endpoint="register", methods=["POST"])
+def register():
+    if flask.request.method == 'POST':
+        username = flask.request.form['username']
+        password = flask.request.form['password']
+        email = flask.request.form['email']
+        dob = flask.request.form['dob']
+        phonenumber = flask.request.form['phonenumber']
 
     db.addUser(username, password, email, dob, phonenumber)
 
-
     """Send register page"""
-    return flask.render_template("index.html")
+    return flask.render_template("register.html")
 
 
 if "__main__" == __name__:
